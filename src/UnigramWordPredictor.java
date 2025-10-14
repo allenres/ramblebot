312bbcd54738521c2ca7515add4362d2de559a43
allenres <allen.resulidze@gmail.com>
2025-10-13 17:42:46 -0700

    test wave 3

--- src/UnigramWordPredictor.java
+++ src/UnigramWordPredictor.java
@@ -52,6 +52,15 @@ public class UnigramWordPredictor implements WordPredictor {
     List<String> trainingWords = tokenizer.tokenize(scanner);
 
     // TODO: Convert the trainingWords into neighborMap here
+    for(int i = 0; i < trainingWords.size(); i++) {
+      if(!neighborMap.containsKey(trainingWords.get(i))) {
+        List<String> newList = new ArrayList<>();
+        newList.add(trainingWords.get(i + 1))
+        neighborMap.put(trainingWords.get(i), newList);
+      } else {
+        neighborMap.get(i).add(trainingWords.get(i + 1));
+      }
+    }
   }
 
   /**
